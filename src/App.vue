1c48c8c669c572038498e4dbc1d901eca458848f
KMarceR <kmarcebr@gmail.com>
2024-10-23 21:53:41 -0600

    Se hicieron los cambios de iconos del menu

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <v-app>
     <!-- Menú lateral -->
     <v-navigation-drawer class="bg-blue-lighten-4" theme="dark" permanent>
-      <v-list-item link to="/" title="Grid" prepend-icon="mdi-grid"></v-list-item>
-      <v-list-item link to="/form" title="Form" prepend-icon="mdi-form-textbox"></v-list-item>
-        <v-list-item link to="/card" title="Cards" prepend-icon="mdi-card"></v-list-item>
-        <v-list-item link to="/alert" title="Alerts" prepend-icon="mdi-bell"></v-list-item>
-        <v-list-item link to="/tabs" title="Tabs" prepend-icon="mdi-tab"></v-list-item>
-        <v-list-item link to="/table" title="Tables" prepend-icon="mdi-table"></v-list-item>
+      <v-list-item link to="/" title="Grid" prepend-icon="mdi-home-outline"></v-list-item>
+      <v-list-item link to="/form" title="Form" prepend-icon="mdi-cart-outline"></v-list-item>
+        <v-list-item link to="/card" title="Cards" prepend-icon="mdi-medication-outline"></v-list-item>
+        <v-list-item link to="/alert" title="Alerts" prepend-icon="mdi-clipboard-edit-outline"></v-list-item>
+        <v-list-item link to="/tabs" title="Tabs" prepend-icon="mdi-account-group-outline"></v-list-item>
+        <v-list-item link to="/table" title="Tables" prepend-icon="mdi-file-chart-outline"></v-list-item>
     </v-navigation-drawer>
     <!-- Carga de componentes -->
     <v-main>
