cac9532cb487100f1b07c0a7bdeb7c985fae4d93
KMarceR <kmarcebr@gmail.com>
2024-10-27 20:48:33 -0600

    Se agrega la seccion de proveedores

---
+++ src/views/Administrador/ProveedoresView.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container :fluid=true>
+        <v-row>
+            <v-col class="text-indigo">
+                <h1>Proveedores</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ProveedoresView'
+} 
+</script>
